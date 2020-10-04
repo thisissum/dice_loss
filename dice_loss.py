09d27f44ee902b58d0ba12871d4b52c1b80843ec
thisissum <55789007+thisissum@users.noreply.github.com>
2020-10-04 18:42:02 +0800

    Add alpha and gamma as hyperparam, fix some bugs

--- dice_loss.py
+++ dice_loss.py
@@ -1,30 +1,70 @@
-import torch 
+import torch
 from torch import nn
+import torch.nn.functional as F
 
-class DiceLoss(nn.Module):
+
+class BinaryDiceLoss(nn.Module):
     """DiceLoss implemented from 'Dice Loss for Data-imbalanced NLP Tasks'
     Useful in dealing with unbalanced data
     Add softmax automatically
     """
 
-    def __init__(self):
-        super(DiceLoss, self).__init__()
-    
-    def forward(self, y_pred, y_true):
-        # shape(y_pred) = batch_size, label_num, **
-        # shape(y_true) = batch_size, **
-        y_pred = torch.softmax(y_pred, dim=1)
-        pred_prob = torch.gather(y_pred, dim=1, index=y_true.unsqueeze(1))
-        dsc_i = 1 - ((1-pred_prob)*pred_prob) / ((1-pred_prob) * pred_prob + 1)
-        dice_loss = dsc_i.mean()
-        return dice_loss
+    def __init__(self, alpha=0.5, gamma=0.5):
+        super(BinaryDiceLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
 
-#=================================TEST=================================
+    def forward(self, y_pred, y_true, reduction='mean'):
+        """
+        :param y_pred: [N, C, ]
+        :param y_true: [N, C, ]
+        :param reduction: 'mean' or 'sum'
+        """
+        batch_size = y_true.size(0)
+        y_pred = y_pred.contiguous().view(batch_size, -1)
+        y_true = y_true.contiguous().view(batch_size, -1)
+
+        numerator = torch.sum(2 * torch.pow((1 - y_pred), self.alpha) * y_pred * y_true, dim=1) + self.gamma
+        denominator = torch.sum(torch.pow((1 - y_pred), self.alpha)  * y_pred + y_true, dim=1) + self.gamma
+        loss = 1 - (numerator / denominator)
+        if reduction == 'mean':
+            return loss.mean()
+        elif reduction == 'sum':
+            return loss.sum()
+        else:
+            return loss
+
+class DiceLoss(nn.Module):
+    def __init__(self, alpha=1, gamma=1):
+        super(DiceLoss, self).__init__()
+        self.alpha = alpha
+        self.gamma = gamma
+        self.binary_dice_loss = BinaryDiceLoss(alpha, gamma)
+
+    def forward(self, y_pred, y_true, reduction='mean'):
+        """
+        :param y_pred: [N, C, ]
+        :param y_true: [N, ]
+        :param reduction: 'mean' or 'sum'
+        """
+        shape = y_pred.shape
+        num_labels = shape[1]
+        dims = [i for i in range(len(y_pred.shape))]
+        # print(dims)
+        dims[1], dims[-1] = dims[-1], dims[1]
+        y_pred = torch.softmax(y_pred, dim=1)
+        y_true = F.one_hot(y_true, num_classes=num_labels).permute(*dims)
+        loss = self.binary_dice_loss(y_pred, y_true, reduction)
+        return loss
+
+
+# =================================TEST=================================
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc = nn.Linear(4,32)
-        self.fc2 = nn.Linear(32,3)
+        self.fc = nn.Linear(4, 32)
+        self.fc2 = nn.Linear(32, 3)
+
     def forward(self, x):
         return self.fc2(self.fc(x))
 
@@ -38,10 +78,10 @@ if __name__ == '__main__':
         X = data['data']
         y = data['target']
         model = Net()
-        opti = torch.optim.Adam(model.parameters())
-        criterion = DiceLoss()
+        opti = torch.optim.Adam(model.parameters(), lr=1e-2)
+        criterion = DiceLoss(0.5,0)
         from torch.utils.data import TensorDataset, DataLoader
-        tx, cvx, ty, cvy = train_test_split(X, y, test_size=0.2)
+        tx, cvx, ty, cvy = train_test_split(X, y, test_size=0.2, random_state=1)
         train_loader = DataLoader(TensorDataset(torch.FloatTensor(tx), torch.LongTensor(ty)), batch_size=32)
         dev_loader = DataLoader(TensorDataset(torch.FloatTensor(cvx), torch.LongTensor(cvy)), batch_size=32)
         for i in range(100):
@@ -55,5 +95,5 @@ if __name__ == '__main__':
                 output = model(dev_x)
                 true_num = (output.argmax(dim=1) == dev_y).sum()
             print(true_num)
-            
+
     test()
